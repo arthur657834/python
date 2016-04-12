dc677141a2fc3d2a6f4ba823584353c21ec2bb89
arthur657834 <kingkom7834@126.com>
2016-04-13 01:09:28 +0800

    daily update

--- json.py
+++ json.py
@@ -14,3 +14,9 @@ print decodejson[0][0]
 print decodejson[1]
 print decodejson[4]['key1']
 print decodejson
+
+obj1={'key1':(1,2,3),'key2':(4,5,6)}
+decodejson1 = json.loads(json.dumps(obj1))
+print "---------------------"
+print decodejson1
+print decodejson1['key1']
